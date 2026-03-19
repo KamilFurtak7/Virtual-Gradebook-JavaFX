ccb31f94b9e16897870a4a331a00a0d116fe479a
Kamil Furtak <kamil.furtak33@gmail.com>
2026-03-19 06:55:03 +0100

    Add dean panel

--- src/main/java/pl/dziennik/virtualgradebookfx/service/impl/UserServiceImpl.java
+++ src/main/java/pl/dziennik/virtualgradebookfx/service/impl/UserServiceImpl.java
@@ -53,4 +53,100 @@ public class UserServiceImpl implements UserService {
 
         return users;
     }
+
+    @Override
+    public List<Teacher> getAllTeachers() {
+        List<Teacher> teachers = new ArrayList<>();
+
+        String sql = "SELECT * FROM users WHERE role = 'TEACHER' ORDER BY last_name, first_name";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                teachers.add(new Teacher(
+                        resultSet.getString("login"),
+                        resultSet.getString("password"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        resultSet.getString("subject_name")
+                ));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return teachers;
+    }
+
+    @Override
+    public void addTeacher(String login, String password, String firstName, String lastName, String subjectName) {
+        String sql = "INSERT INTO users (login, password, first_name, last_name, role, school_class, subject_name) VALUES (?, ?, ?, ?, 'TEACHER', NULL, ?)";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, login);
+            statement.setString(2, password);
+            statement.setString(3, firstName);
+            statement.setString(4, lastName);
+            statement.setString(5, subjectName);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateTeacher(String login, String password, String firstName, String lastName, String subjectName) {
+        String sql;
+        boolean updatePassword = password != null && !password.isBlank();
+
+        if (updatePassword) {
+            sql = "UPDATE users SET password = ?, first_name = ?, last_name = ?, subject_name = ? WHERE login = ? AND role = 'TEACHER'";
+        } else {
+            sql = "UPDATE users SET first_name = ?, last_name = ?, subject_name = ? WHERE login = ? AND role = 'TEACHER'";
+        }
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            if (updatePassword) {
+                statement.setString(1, password);
+                statement.setString(2, firstName);
+                statement.setString(3, lastName);
+                statement.setString(4, subjectName);
+                statement.setString(5, login);
+            } else {
+                statement.setString(1, firstName);
+                statement.setString(2, lastName);
+                statement.setString(3, subjectName);
+                statement.setString(4, login);
+            }
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteTeacher(String login) {
+        String sql = "DELETE FROM users WHERE login = ? AND role = 'TEACHER'";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, login);
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/pl/dziennik/virtualgradebookfx/service/interfaces/UserService.java
+++ src/main/java/pl/dziennik/virtualgradebookfx/service/interfaces/UserService.java
@@ -1,9 +1,14 @@
 package pl.dziennik.virtualgradebookfx.service.interfaces;
 
+import pl.dziennik.virtualgradebookfx.model.user.Teacher;
 import pl.dziennik.virtualgradebookfx.model.user.User;
 
 import java.util.List;
 
 public interface UserService {
     List<User> getAllUsers();
+    List<Teacher> getAllTeachers();
+    void addTeacher(String login, String password, String firstName, String lastName, String subjectName);
+    void updateTeacher(String login, String password, String firstName, String lastName, String subjectName);
+    void deleteTeacher(String login);
 }
